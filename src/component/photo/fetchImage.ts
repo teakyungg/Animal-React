b496bc47710c27bd96e87efab6d923ad76827365
teakyungg <kak0502@naver.com>
2025-12-19 01:18:56 +0900

    feat/fetchImage.ts 이미지 갯수 변수 추가

--- src/component/photo/fetchImage.ts
+++ src/component/photo/fetchImage.ts
@@ -2,14 +2,15 @@
 
 export async function fetchImage(type: string | null) {
   const serverUrl = import.meta.env.VITE_APP_SERVER_URL; // 서버 주소
+  const imageLength = 10;
   let res;
 
   if (type == null || type == "Random") {
     type = "random";
-    res = await fetch(`${serverUrl}/breeds/image/${type}/5`);
+    res = await fetch(`${serverUrl}/breeds/image/${type}/${imageLength}`);
   } else {
     const typeText = type[0].toLowerCase() + type.slice(1);
-    res = await fetch(`${serverUrl}/breed/${typeText}/images/random/5`);
+    res = await fetch(`${serverUrl}/breed/${typeText}/images/random/${imageLength}`);
   }
 
   if (!res.ok) {
