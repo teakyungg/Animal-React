64fcf32fea470e3f064116f7c79a3a2f655bc351
teakyungg <kak0502@naver.com>
2025-12-20 19:54:50 +0900

    feat/fetchImage.tsx 주석 추가

--- src/section/photo/fetchImage.ts
+++ src/section/photo/fetchImage.ts
@@ -4,10 +4,14 @@ export async function fetchImage(type: string | null) {
   const imageLength = 10;
   let res;
 
+  // 입력값이 없다면 기본으로 랜덤 이미지를 가져옮
   if (type == null || type == "Random") {
     type = "random";
     res = await fetch(`${serverUrl}/breeds/image/${type}/${imageLength}`);
-  } else {
+  }
+
+  // 특정 입력값이 있다면 입력값 기반으로 이미지를 가져옮
+  else {
     const typeText = type[0].toLowerCase() + type.slice(1);
     res = await fetch(`${serverUrl}/breed/${typeText}/images/random/${imageLength}`);
   }
@@ -16,6 +20,7 @@ export async function fetchImage(type: string | null) {
     throw new Error("HTTP 에러");
   }
 
+  // 데이터 변환
   const data = await res.json();
   return data;
 }
