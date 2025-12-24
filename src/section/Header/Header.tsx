2e0ded2eba9e9ea1b57dffb25edc4a0341af0205
teakyungg <kak0502@naver.com>
2025-12-24 18:57:42 +0900

    feat/Header.tsx 검색 기능 추가, Photo.tsx 파일 삭제

---
+++ src/section/Header/Header.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useRef, useState } from "react";
+import "./Header.css";
+import { TypeButton } from "../../component/TypeButton/TypeButton";
+import userData from "../../userData";
+
+export function Header() {
+  const serverUrl = import.meta.env.VITE_APP_SERVER_URL; // 서버 주소
+  const dogTypes = useRef<string[]>([]); // 품종 데이터
+  const [buttonData, setButtonData] = useState<string[]>([]); // 버튼 데이터
+
+  const inputRef = useRef<HTMLInputElement>(null); // input 초기화용
+  const selectType = userData((state) => state.selectType); // 이미지 변경시 실행할 함수
+
+  // 검색 조건 탐색 로직
+  const searchType = (input: string) => {
+    const text = input.trim().toLowerCase();
+    if (text.length === 0) {
+      setButtonData([]);
+      return;
+    }
+
+    const found = dogTypes.current.filter((k) => k.includes(text)).slice(0, 8);
+    setButtonData(found);
+  };
+
+  useEffect(() => {
+    async function setType() {
+      const res = await fetch(`${serverUrl}/breeds/list/all`);
+      const data = await res.json();
+      dogTypes.current = Object.keys(data.message);
+    }
+    setType();
+  }, []);
+
+  return (
+    <header>
+      <div className="inner">
+        <img src="/logo.svg" alt="animal logo" className="logo" />
+
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="원하는 품종을 검색해주세요"
+          onChange={(e) => searchType(e.target.value)}
+        />
+      </div>
+
+      {buttonData.length > 0 && (
+        <div className="photoType">
+          {buttonData.map((value) => (
+            <TypeButton
+              key={value}
+              onClick={() => {
+                selectType(value);
+                setButtonData([]);
+                if (inputRef.current) {
+                  inputRef.current.value = "";
+                }
+              }}
+            >
+              {value}
+            </TypeButton>
+          ))}
+        </div>
+      )}
+    </header>
+  );
+}
