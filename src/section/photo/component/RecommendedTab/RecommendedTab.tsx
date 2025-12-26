70f09b7919463c615645bcd00512706e36272240
teakyungg <kak0502@naver.com>
2025-12-26 14:44:03 +0900

    feat/RecommendedTab.tsx 강아지 타입 데이터 가져오는 방식 변경

--- src/section/photo/component/RecommendedTab/RecommendedTab.tsx
+++ src/section/photo/component/RecommendedTab/RecommendedTab.tsx
@@ -1,27 +1,39 @@
+import { useEffect, useState } from "react";
 import { TypeButton } from "../../../../component/TypeButton/TypeButton";
 import useData from "../../../../userData";
-import "./RecommendedTab.css";
 import userData from "../../../../userData";
+import "./RecommendedTab.css";
 
-// 추천 탭
+// 전체 커밋찍기
+
 export default function RecommendedTab() {
-  const allDogType = useData((state) => state.allDogType); // 모든 강아지의 타입
-  const activeType = useData((state) => state.activeType); // 현재 클릭한 개의 품종
+  const allDogType = useData((state) => state.allDogType); // 모든 강아지의 타입 (이거 값 다른 곳에서 await 형태로 세팅됨)
+  const activeType = userData((state) => state.activeType); // 현재 클릭한 타입
   const selectType = userData((state) => state.selectType); // 이미지 변경시 실행할 함수
 
-  const typeItem = allDogType.map((value) => {
-    if (activeType !== value) {
-      return (
-        <TypeButton onClick={() => selectType(value)} key={`Recommended Button ${value}`}>
-          {value}
-        </TypeButton>
-      );
-    }
-  });
+  const [randomArr, setRandomArr] = useState<string[]>([]);
+
+  // 원래 useEffect 안써도 되지만 렌더링 전에 Math.random 라는  비순수 함수를 사용하는게
+  // 권장되지 않기에 일부로 useEffect를 사용함
+  useEffect(() => {
+    if (allDogType.length <= 2) return;
+    setRandomArr([...allDogType].sort(() => 0.5 - Math.random()).slice(0, 3));
+  }, [allDogType, activeType]);
+
+  const typeItem = randomArr.map((value, index) => (
+    <TypeButton
+      key={`RecommendedTab ${index}`}
+      onClick={() => {
+        selectType(value);
+      }}
+    >
+      {value}
+    </TypeButton>
+  ));
 
   return (
     <div className="images RecommendedTab">
-      <div className="sub-title">이런 강아지는 어떠세요?</div>
+      <div>이런 강아지는 어떠세요?</div>
       <div className="title">추천 강아지</div>
       <div className="types">{typeItem}</div>
     </div>
