0a60f0fbf56b7d6fac0dc7b6b032fe4201f2ae56
teakyungg <kak0502@naver.com>
2025-12-27 18:17:50 +0900

    refactor/App.tsx 사용하지 않는 부분 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { Header } from "./section/Header/Header";
-import { Suspense, lazy } from "react";
+import { lazy } from "react";
 
 // lazy : 컴포넌트를 지금 당장 안 불러오고, 필요할 때 나중에 불러오게 해줌
-// Suspense : lazy로 불러오는 컴포넌트가 아직 로드 중일 때 대신 보여줄 화면(fallback)을 정하는 컴포넌트.
+// FCP를 위한 코드
 const Photo = lazy(() => import("./section/Photo/Photo"));
 
 function App() {
   return (
     <>
       <Header />
-      <Suspense fallback={null}>
-        <Photo />
-      </Suspense>
+      <Photo />
     </>
   );
 }
